4b46c581d3c26420e9ac63eadee635d117df77bb
BoualamBillel <boualambillelsofien@gmail.com>
2026-04-29 11:51:08 +0200

    add edit route + edit view

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -43,4 +43,30 @@ final class ClientController extends AbstractController
             'clients' => $clients
         ]);
     }
+
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
+    public function update(Request $request, Client $client, EntityManagerInterface $em): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        if ($client->getOwner() !== $this->getUser()) {
+            throw $this->createAccessDeniedException('Ce client ne vous appartient pas.');
+        }
+
+        $form = $this->createForm(ClientType::class, $client);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            $this->addFlash('success', 'Le client a été modifié avec succès.');
+
+            return $this->redirectToRoute('app_client_index');
+        }
+
+        return $this->render('client/edit.html.twig', [
+            'form' => $form->createView(),
+            'client' => $client
+        ]);
+    }
 }
