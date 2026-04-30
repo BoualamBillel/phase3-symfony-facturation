3de1b71b220be8be6163824a998799d7b08e1eb3
BoualamBillel <boualambillelsofien@gmail.com>
2026-04-30 11:39:34 +0200

    add delete, validate, pay, show, send routes + show view

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -42,6 +42,20 @@ final class InvoiceController extends AbstractController
         ]);
     }
 
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
+    public function show(Invoice $invoice): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        if ($invoice->getOwner() !== $this->getUser()) {
+            throw $this->createAccessDeniedException("Vous n'avez pas le droit d'accéder à cette facture.");
+        }
+
+        return $this->render('/invoice/show.html.twig', [
+            'invoice' => $invoice,
+        ]);
+    }
+
     #[Route('/add', name: 'add', methods: ['GET', 'POST'])]
     public function add(Request $request, EntityManagerInterface $em): Response
     {
@@ -157,5 +171,91 @@ final class InvoiceController extends AbstractController
             'form' => $form->createView(),
             'products' => $products,
         ]);
+    }
+
+    #[Route('/{id}/delete', name: 'delete', methods: ['POST'])]
+    public function delete(Request $request, Invoice $invoice, EntityManagerInterface $em): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        if ($invoice->getOwner() !== $this->getUser()) {
+            throw $this->createAccessDeniedException();
+        }
+
+        if ($this->isCsrfTokenValid('delete' . $invoice->getId(), $request->request->get('_token'))) {
+            if ($invoice->getStatus() !== 'draft') {
+                $this->addFlash('error', 'Une facture validée ne peut pas être supprimée.');
+                return $this->redirectToRoute('app_invoice_show', ['id' => $invoice->getId()]);
+            }
+
+            $em->remove($invoice);
+            $em->flush();
+            $this->addFlash('success', 'Le brouillon a été supprimé.');
+        } else {
+            $this->addFlash('error', 'Token de sécurité invalide.');
+        }
+
+        return $this->redirectToRoute('app_invoice_index');
+    }
+
+    #[Route(path: '/{id}/validate', name: 'validate', methods: ['POST'])]
+    public function validate(Request $request, Invoice $invoice, EntityManagerInterface $em): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        if ($invoice->getOwner() !== $this->getUser()) {
+            throw $this->createAccessDeniedException();
+        }
+
+        if ($invoice->getStatus() !== 'draft') {
+            $this->addFlash('error', 'Seul un brouillon peut être validé.');
+            return $this->redirectToRoute('app_invoice_show', ['id' => $invoice->getId()]);
+        }
+
+        $invoice->setStatus('validated');
+
+        if (!$invoice->getNumber()) {
+            $invoice->setNumber('FACT-' . date('Y') . '-' . str_pad(rand(1, 999), 3, '0', STR_PAD_LEFT));
+        }
+
+        $em->flush();
+
+        $this->addFlash('success', 'La facture a été validée avec succès. Elle ne peut plus être modifiée.');
+        return $this->redirectToRoute('app_invoice_show', ['id' => $invoice->getId()]);
+    }
+
+    #[Route('/{id}/pay', name: 'pay', methods: ['POST'])]
+    public function pay(Invoice $invoice, Request $request, EntityManagerInterface $em): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        if ($invoice->getOwner() !== $this->getUser()) {
+            throw $this->createAccessDeniedException();
+        }
+
+        if ($invoice->getStatus() !== 'validated') {
+            $this->addFlash('error', 'Seule une facture en attente peut être marquée comme payée.');
+            return $this->redirectToRoute('app_invoice_show', ['id' => $invoice->getId()]);
+        }
+
+        $invoice->setStatus('paid');
+        $em->flush();
+
+        $this->addFlash('success', 'Félicitations, le paiement a été enregistré avec succès !');
+        return $this->redirectToRoute('app_invoice_show', ['id' => $invoice->getId()]);
+    }
+
+    #[Route('/{id}/send', name: 'send', methods: ['POST'])]
+    public function send(Invoice $invoice): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        if ($invoice->getOwner() !== $this->getUser()) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $this->addFlash('info', 'Fonctionnalité d\'envoi par email en cours de développement.');
+
+        return $this->redirectToRoute('app_invoice_show', ['id' => $invoice->getId()]);
     }
 }
